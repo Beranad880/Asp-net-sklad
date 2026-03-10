c62b5893e72ce3827fd3121e7474f37d5ae7c5e1
Adam Pokorný <adamhumble@outlook.cz>
2026-03-10 09:51:36 +0100

    feat: Adding App.use middleware

--- WebApplicationASP1/Program.cs
+++ WebApplicationASP1/Program.cs
@@ -1,6 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+app.Use(async (context, next) =>
+{
 
+    Console.WriteLine("Request začal");
+
+    await next(); 
+
+    Console.WriteLine("Response se vrací");
+
+}); ;
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
